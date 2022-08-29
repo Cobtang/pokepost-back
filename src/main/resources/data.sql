2fe90d9923a44b6d008bfb54615e19ecad468c2c
Colby Tang <cobtang@gmail.com>
2022-08-29 02:57:48 -0500

    Added DML for DDL auto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into users (id, username, email, ) values (
+    default, 
+    'ctang', 
+    'colby051@revature.net', 
+    'admin', 
+    'pass', 
+    null, 
+    default, 
+    default
+);
